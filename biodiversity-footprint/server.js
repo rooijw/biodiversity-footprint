91c059d6834bf8c6a58133b3008b95bce2cadfee
plansup <plansup.consult@gmail.com>
2024-03-31 17:26:29 +0200

    trying to get heroku to work

--- biodiversity-footprint/server.js
+++ biodiversity-footprint/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/biodiversity-footprint2'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'./dist/index.html'));});
-app.listen(process.env.PORT || 4200);
+'./dist/biodiversity-footprint2/index.html'));});
+app.listen(process.env.PORT || 8080);
